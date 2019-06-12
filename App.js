244121e672cce2a1ddbdd68596e3d9e5125485c3
iwanttobealight <iwanttobealight@yandex.ru>
2019-06-12 20:32:52 +0300

    feat(app): add background

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { h, render, Component } from "preact";
 import Clock from "./src/clock";
 import "normalize.css";
+import images from "./src/images";
+import fs from "fs";
 
 import "./App.css";
+
+const buffer = fs.readFileSync(__dirname + "/src/images/background.png");
+
 export class App extends Component {
   render() {
-    return <Clock />;
+    console.log("imageURL:", buffer);
+
+    return (
+      <div className="container">
+        <img
+          class="background"
+          src={`data:image/png;base64,${buffer.toString("base64")}`}
+        />
+        <Clock />
+      </div>
+    );
   }
 }
